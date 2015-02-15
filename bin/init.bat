af51654f488725838d38a206a640f0e7b40fdd0a
Debabrata Tripathy <dtripathy10@gmail.com>
2015-02-15 16:05:02 +0530

    testint

---
+++ bin/init.bat
@@ -0,0 +1,10 @@
+PATH=%PATH%;C:\Program Files (x86)\Git\bin
+
+
+echo "------START----------"
+
+git config --global user.email "dtripathy10@gmail.com"
+git config --global user.name "Debabrata Tripathy"
+
+
+git init && git add . && git commit -m "testint"
---
+++ build_system.markdown
@@ -0,0 +1,14 @@
+
+Tools -> Build System
+
++ `command: ` `Ctrl+B` 
+
+```json
+{
+
+	"cmd": ["php","-l",  "$file"]
+}
+```
++ Change the build system
+
++ save into the build directory

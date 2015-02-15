c2c4be38abd2b2d15b759b83cf60881f4390ca69
Debabrata Tripathy <dtripathy10@gmail.com>
2015-02-15 17:12:00 +0530

    Inital Repository

--- bin/init.bat
+++ bin/init.bat
@@ -10,13 +10,18 @@
 :: ################################################################################
 
 
-PATH=%PATH%;C:\Program Files (x86)\Git\bin
+PATH=%PATH%;C:\Program Files (x86)\Git\bin;C:\Python27
 
+@echo off
 
 echo "------START----------"
 
-git config --global user.email "dtripathy10@gmail.com"
-git config --global user.name "Debabrata Tripathy"
+:: git config --global user.email "dtripathy10@gmail.com"
+:: git config --global user.name "Debabrata Tripathy"
 
 
-git init && git add . && git commit -m "testint"
+:: git init && git add . && git commit -m "testint"
+
+python src/main.py
+
+echo "------END----------"
--- docs/build_system.markdown
+++ docs/build_system.markdown
@@ -13,6 +13,8 @@ Tools -> Build System
 
 + save into the build directory
 
++ set the build system in project setting file
+
 ### Reference
 
 + http://sublimetext.info/docs/en/reference/build_systems.html
